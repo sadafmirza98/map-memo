5351d19d9f50866c25b42353e4e28ab25d1dbc08
sadafmirza98 <sadafmirza98@gmail.com>
2024-03-03 21:36:19 +0530

    Pushed files

---
+++ frontend/src/shared/components/FormElements/ImageUpload.js
@@ -0,0 +1,60 @@
+import React, { useRef, useState, useEffect } from "react";
+import "./ImageUpload.css";
+import Button from "./Button";
+const ImageUpload = (props) => {
+  const filePickerRef = useRef();
+  const [file, setFile] = useState();
+  const [previewUrl, setPreviewUrl] = useState();
+  const [isValid, setIsValid] = useState(false);
+  useEffect(() => {
+    if (!file) {
+      return;
+    }
+    const fileReader = new FileReader();
+    fileReader.onload = () => {
+      setPreviewUrl(fileReader.result);
+    };
+    fileReader.readAsDataURL(file);
+  }, [file]);
+  const pickedHandler = (event) => {
+    let pickedFile;
+    let fileIsValid = isValid;
+    if (event.target.files && event.target.files.length === 1) {
+      const pickedFile = event.target.files[0];
+      setFile(pickedFile);
+      setIsValid(true);
+      fileIsValid = true;
+    } else {
+      setIsValid(false);
+      fileIsValid = false;
+    }
+    props.onInput(props.id, pickedFile, fileIsValid);
+  };
+  const pickImageHandler = () => {
+    filePickerRef.current.click();
+  };
+  return (
+    <div className="form-control">
+      <input
+        id={props.id}
+        ref={filePickerRef}
+        style={{ display: "none" }}
+        type="file"
+        accept=".jpg, .png, .jpeg"
+        onChange={pickedHandler}
+      ></input>
+      <div className={`image-upload ${props.center && "center"}`}>
+        <div className="image-upload__preview">
+          {previewUrl && <img src={previewUrl} alt="preview" />}
+          {!previewUrl && <p>Please pick an Image.</p>}
+        </div>
+        <Button type="button" onClick={pickImageHandler}>
+          PICK BUTTON
+        </Button>
+      </div>
+      {!isValid && <p>{props.errorText}</p>}
+    </div>
+  );
+};
+
+export default ImageUpload;
---
+++ frontend/src/shared/components/Navigation/MainNavigation.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+import MainHeader from "./MainHeader";
+import NavLinks from "./NavLinks";
+import SideDrawer from "./SideDrawer";
+import Backdrop from "../UIElements/Backdrop";
+import logo from "../../assets/logo.svg";
+import "./MainNavigation.css";
+
+const MainNavigation = (props) => {
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+  const openDrawerHandler = () => {
+    setDrawerIsOpen(true);
+  };
+
+  const closeDrawerHandler = () => {
+    setDrawerIsOpen(false);
+  };
+
+  return (
+    <React.Fragment>
+      {drawerIsOpen && <Backdrop onClick={closeDrawerHandler} />}
+      <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
+        <nav className="main-navigation__drawer-nav">
+          <NavLinks />
+        </nav>
+      </SideDrawer>
+
+      <MainHeader>
+        <button
+          className="main-navigation__menu-btn"
+          onClick={openDrawerHandler}
+        >
+          <span />
+          <span />
+          <span />
+        </button>
+
+        <div className="main-navigation__title">
+          <div>
+            <img src={logo} className="img-fluid logo" alt="brand" />
+          </div>
+          <Link to="/">
+            <div className="logo-text">MapMemo</div>
+          </Link>
+        </div>
+        <nav className="main-navigation__header-nav">
+          <NavLinks />
+        </nav>
+      </MainHeader>
+    </React.Fragment>
+  );
+};
+
+export default MainNavigation;
